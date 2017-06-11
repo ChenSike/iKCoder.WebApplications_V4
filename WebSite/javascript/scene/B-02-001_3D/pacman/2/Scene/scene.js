3d3abbee0a966fa274cbe30b0153f0c8f7a327ef
Visen <visen49501@gmail.com>
2017-06-11 14:49:57 +0800

    Clean up code.

--- WebSite/javascript/scene/B-02-001_3D/pacman/2/Scene/scene.js
+++ WebSite/javascript/scene/B-02-001_3D/pacman/2/Scene/scene.js
@@ -4,24 +4,31 @@ var Scene = {};
 
 Scene.defaultDATA = [
 	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	[1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-	[1, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-	[1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-	[1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-	[1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1],
-	[1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-	[1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1],
-	[1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
-	[1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
-	[1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
-	[1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1],
-	[1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+	[1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
+	[1, 0, 0, 0, 1, 0, 0, 6, 0, 1, 0, 0, 0, 1],
+	[1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1],
+	[1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1],
+	[1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1],
+	[1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 2, 1],
+	[1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1],
+	[1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1],
+	[1, 0, 0, 0, 1, 0, 0, 6, 0, 1, 0, 0, 0, 1],
+	[1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
 	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
+Scene.isFirstMonster = false;
+
 Scene.mapDATA = [];
 Scene._CALCMOVEPATH = [];
-Scene.targetPos = { x: 2, y: 2 };
+Scene.targetPos = { x: 12, y: 7 };
+Scene.randomObj = [];
+ Scene.monsterParams = [
+	{ x: 7, y: 3, c: null },
+	{ x: 7, y: 10, c: null }
+];
 
 Scene.initEnvironment = function (containerId) {
 	Scene.initGlobalParams();
@@ -82,7 +89,8 @@ Scene.initEnvironment = function (containerId) {
 	Engine.initScreenAnd3D(containerId, params);
 	Engine.prepareForStart();
 	Scene.initMap();
-	Scene.initPlayer(12, 12);
+	Scene.initPlayer(1, 7);
+
 };
 
 Scene.initMap = function () {
@@ -90,6 +98,7 @@ Scene.initMap = function () {
 	var halfWidth = _colCount * _itemSize / 2;
 	var halfHeight = _rowCount * _itemSize / 2;
 	var goods = [];
+	var questionMarkCount = 0;
 	for (var i = 0; i < Scene.defaultDATA.length; i++) {
 		tmpZ = i * _itemSize - halfWidth + _itemSize / 2;
 		var rowData = [];
@@ -104,6 +113,23 @@ Scene.initMap = function () {
 				var newGoods = new Goods();
 				goods.push(newGoods);
 				tmpKey = Engine.addModuleObject(newGoods, tmpX, null, tmpZ);
+			} else if (Scene.defaultDATA[i][j] == 6) {
+
+				questionMarkCount++;
+
+				var tmpBean = new Bean();
+				tmpBean.type = 'especialBean';
+
+				tmpBean.mesh.visible = false;
+
+				var tmpQuestionMark = new QuestionMark();
+
+				Engine.addModuleObject(tmpQuestionMark, tmpX, null, tmpZ);
+				tmpKey = Engine.addModuleObject(tmpBean, tmpX, null, tmpZ);
+
+				Scene.randomObj.push({ name: "questionmark" + questionMarkCount, obj: tmpQuestionMark });
+				Scene.randomObj.push({ name: "bean" + questionMarkCount, obj: tmpBean });
+
 			}
 
 			rowData.push({ t: Scene.defaultDATA[i][j], s: tmpKey, v: true });
@@ -113,10 +139,17 @@ Scene.initMap = function () {
 	}
 
 	Goods.updatePose(goods);
+
+	Scene.initMonster(Scene.monsterParams );
+
+	for (var i = 4; i < Scene.randomObj.length; i++) {
+		Scene.randomObj[i].obj.mesh.visible = false;
+	}
 };
 
 Scene.initPlayer = function (x, y) {
 	var player = new PACMan('study', Scene.mapDATA);
+	player._stopWhenComplete = false;
 	Engine.addModuleObject(player, x, null, y);
 	player.setPosition(x, y);
 	if (Scene.mapDATA[y][x].t == 0 || Scene.mapDATA[y][x].t == 2) {
@@ -131,6 +164,26 @@ Scene.initPlayer = function (x, y) {
 			Scene.stepFaild();
 		}
 	});
+};
+
+Scene.initMonster = function (params) {
+	var monsters = [];
+
+	for (var i = 0; i < params.length; i++) {
+		var monster = new Monster('study', Scene.mapDATA);
+		monsters.push(monster);
+		Engine.addModuleObject(monster, params[i].x, null, params[i].y);
+		monster.setPosition(params[i].x, params[i].y);
+		if (Scene.mapDATA[params[i].y][params[i].x].t == 0 || Scene.mapDATA[params[i].y][params[i].x].t == 2) {
+			Engine.getModuleObject(Scene.mapDATA[params[i].y][params[i].x].s).mesh.visible = false;
+		}
+
+		Scene.mapDATA[params[i].y][params[i].x].v = false;
+
+		Scene.randomObj.push({ name: 'monster' + i, obj: monster });
+	}
+
+	Monster.updatePose(monsters);
 };
 
 Scene.initGlobalParams = function () {
@@ -165,11 +218,54 @@ Scene.start = function () {
 	Engine.startScene();
 };
 
+Scene.setRandomObjectStaus = function () {
+
+	var initRandomValue = Math.floor(Math.random() * 2);
+
+	Scene.randomObj[0].obj.mesh.visible = false;
+	Scene.randomObj[2].obj.mesh.visible = false;
+
+	if (initRandomValue == 0) {
+		Scene.randomObj[1].obj.mesh.visible = true;
+		Scene.randomObj[3].obj.mesh.visible = false;
+		Scene.randomObj[4].obj.mesh.visible = false;
+		Scene.randomObj[5].obj.mesh.visible = true;
+		Scene.isFirstMonster = false;
+
+	} else {
+		Scene.randomObj[1].obj.mesh.visible = false;
+		Scene.randomObj[3].obj.mesh.visible = true;
+		Scene.randomObj[4].obj.mesh.visible = true;
+		Scene.randomObj[5].obj.mesh.visible = false;
+		Scene.isFirstMonster = true;
+	}
+
+};
+
+Scene.setRandomObjectOnlyQuestionMarkVisible = function () {
+	Scene.randomObj[0].obj.mesh.visible = true;
+	Scene.randomObj[2].obj.mesh.visible = true;
+
+	Scene.randomObj[1].obj.mesh.visible = false;
+	Scene.randomObj[3].obj.mesh.visible = false;
+	Scene.randomObj[4].obj.mesh.visible = false;
+	Scene.randomObj[5].obj.mesh.visible = false;
+};
+
+Scene.ResetConfig = function () {
+	Scene.setRandomObjectStaus();
+	Scene._CALCMOVEPATH = [];
+};
+
 Scene.reset = function () {
 	Scene._CALCMOVEPATH = [];
+
 	Engine.resetScene();
 	Scene.resetMap();
+	Scene.resetRandomObj();
+
 	Scene.getPlayer().reset();
+
 	var x = Scene.getPlayer().defaultCoord.x;
 	var y = Scene.getPlayer().defaultCoord.y;
 	if (Scene.mapDATA[y][x].t == 0 || Scene.mapDATA[y][x].t == 2) {
@@ -177,13 +273,33 @@ Scene.reset = function () {
 	}
 
 	Scene.mapDATA[y][x].v = false;
-
 };
 
 Scene.resetMap = function () {
 	for (var i = 0; i < Scene.mapDATA.length; i++) {
 		for (var j = 0; j < Scene.mapDATA[i].length; j++) {
 			Scene.mapDATA[i][j].v = true;
+		}
+	}
+};
+
+Scene.resetRandomObj = function () {
+
+	for (var key in Engine.modules) {
+
+		switch (Engine.modules[key].type) {
+			case "questionmark":
+				Engine.modules[key].mesh.visible = true;
+				Engine.modules[key]
+				break;
+
+			case "monster":
+				Engine.modules[key].mesh.visible = false;
+				break;
+
+			case "especialBean":
+				Engine.modules[key].mesh.visible = false;
+				break;
 		}
 	}
 };
@@ -197,7 +313,6 @@ Scene.resetSize = function () {
 	}
 };
 
-
 Scene.move = function (steps) {
 	Scene.addModuelPath('pacman', 'm', steps);
 
@@ -207,7 +322,7 @@ Scene.move = function (steps) {
 
 	if (Scene._CALCMOVEPATH.length > 0) {
 		curOrientation = Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].orientation;
-		curX = Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].x;
+		curX = Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].x ;
 		curY = Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].y;
 	}
 
@@ -231,7 +346,6 @@ Scene.move = function (steps) {
 
 	Scene._CALCMOVEPATH.push(pathItemOri);
 };
-
 
 Scene.TurnLeft = function (output) {
 	if (output === true) {
@@ -333,51 +447,49 @@ Scene.moveForward = function () {
 };
 
 Scene.isWall = function () {
+	return true;
 	var nextX, nextY, currentX, currentY, currentOrientation;
+
 	if (Scene._CALCMOVEPATH.length != 0) {
 		currentX = Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].x;
 		currentY = Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].y;
 		currentOrientation = Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].orientation;
 	}
+
 	switch (currentOrientation) {
 		case 0:
-			nextX = currentX + 1;
-			nextY = currentY;
-			break;
-		case 2:
-			nextX = currentX - 1;
-			nextY = currentY;
+			var nextX = currentX + 1;
+			var nextY = currentY;
 			break;
 		case 1:
-			nextX = currentX;
-			nextY = currentY - 1;
+			var nextX = currentX;
+			var nextY = currentY - 1;
+			break;
+		case 2:
+			var nextX = currentX - 1;
+			var nextY = currentY;
 			break;
 		case 3:
-			nextX = currentX;
-			nextY = currentY + 1;
+			var nextX = currentX;
+			var nextY = currentY + 1;
 			break;
 	}
-
-	if (Scene.checkPointUsedByWall(nextX, nextY)) {
+	if (Scene.mapDATA[nextY][nextX].t == 1) {
 		return true;
-	}
-	else {
+	} else {
 		return false;
 	}
-};
 
-Scene.checkPointUsedByWall = function (x, y) {
-	if (Scene.defaultDATA[y][x] == 1) {
-		return true;
-	}
-
-	return false;
 };
 
 Scene.startGame = function () {
-	Scene.start();
-};
 
+	var code = Blockly.JavaScript.workspaceToCode(WorkScene.workspace);
+
+	if (Scene.checkBlockly(code)) {
+		Scene.start();
+	}
+};
 
 Scene.setFunctionWhenWall = function (logicStr) {
 	var arr = logicStr.split(';');
@@ -400,32 +512,174 @@ Scene.setFunctionWhenWall = function (logicStr) {
 };
 
 Scene.NotEatRedBeans = function () {
-
-	var nextX, nextY, currentX, currentY, currentOrientation;
-	var pathItemOri = {
-		x: Scene.getPlayer().coord.x,
-		y: Scene.getPlayer().coord.y,
-		orientation: Scene.getPlayer().orientation
-	};
+	var nextX, nextY, currentX, currentY;
+	var pathItemOri;
 
 	if (Scene._CALCMOVEPATH.length == 0) {
-		Scene._CALCMOVEPATH.push(pathItemOri);
+
+		pathItemOri = {
+			x: Scene.getPlayer().coord.x,
+			y: Scene.getPlayer().coord.y
+		};
+
 		currentX = Scene.getPlayer().coord.x;
 		currentY = Scene.getPlayer().coord.y;
+		Scene._CALCMOVEPATH.push(pathItemOri);
+
 	} else {
+		pathItemOri = {
+			x: Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].x + 1,
+			y: Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].y
+		};
+
+		Scene._CALCMOVEPATH.push(pathItemOri);
+
 		currentX = Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].x;
 		currentY = Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].y;
 	}
 
-var targetPos = Scene.targetPos;
-	if (Scene.isWall()) {
+	var targetPos = Scene.targetPos;
+
+	if (targetPos.x == currentX && targetPos.y == currentY) {
 		return false;
 	} else {
-		if (targetPos.x == currentX && targetPos.y == currentY) {
-			return false;
-		} else {
-			return true;
+		return true;
+	}
+
+};
+
+Scene.checkBlockly = function (code) {
+	var result = true;
+
+	var strtmp = code.replace(/"/, "\"");
+	var strtmp1 = strtmp.replace(/  /, '');
+	var strtmp2 = strtmp1.replace(/ /, '');
+	var sourceStr = strtmp2.replace(/\n/g, '');
+
+	var x1 = findStringPostion(sourceStr, "{", 0) + 1;
+	var x2 = findStringPostion(sourceStr, "{", 1) + 1;
+	var y1 = findStringPostion(sourceStr, "}", 0);
+	var y2 = findStringPostion(sourceStr, "}", 1);
+
+	var ifx = sourceStr.indexOf("if");
+	var tarStr1 = sourceStr.substring(x1, y1);
+	var tarStr2 = sourceStr.substring(x2, y2);
+
+	if (ifx == -1) {
+		Scene.setRandomObjectOnlyQuestionMarkVisible();
+		Scene.stepFaild();
+		result = false;
+	} else {
+		if (tarStr1 == "") {
+			Scene.setRandomObjectOnlyQuestionMarkVisible();
+			Scene.stepFaild();
+			result = false;
+		} else if (tarStr2 == "") {
+			Scene.setRandomObjectOnlyQuestionMarkVisible();
+			Scene.stepFaild();
+			result = false;
 		}
+	}
+
+	return result;
+};
+
+function findStringPostion(searchString, searchedString, num) {
+	var x = searchString.indexOf(searchedString);
+	for (var i = 0; i < num; i++) {
+		x = searchString.indexOf(searchedString, x + 1);
+	}
+	return x;
+};
+
+Scene.isBean = function () {
+	var nextX, nextY, currentX, currentY, currentOrientation;
+	if (Scene._CALCMOVEPATH.length != 0) {
+		currentX = Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].x;
+		currentY = Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].y;
+		currentOrientation = Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].orientation;
+	} else {
+		var player = Scene.getPlayer();
+		currentX = player.coord.x;
+		currentY = player.coord.y;
+		currentOrientation = player.orientation;
+	}
+	switch (currentOrientation) {
+		case 0:
+			nextX = currentX + 1;
+			nextY = currentY;
+			break;
+		case 2:
+			nextX = currentX - 1;
+			nextY = currentY;
+			break;
+		case 1:
+			nextX = currentX;
+			nextY = currentY - 1;
+			break;
+		case 3:
+			nextX = currentX;
+			nextY = currentY + 1;
+			break;
+	}
+	if (Scene.checkPointUsedByBean(nextX, nextY)) {
+		return true;
+	} else {
+		return false;
 	}
 };
 
+Scene.isMonster = function () {
+	var nextX, nextY, currentX, currentY, currentOrientation;
+	if (Scene._CALCMOVEPATH.length != 0) {
+		currentX = Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].x;
+		currentY = Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].y;
+		currentOrientation = Scene._CALCMOVEPATH[Scene._CALCMOVEPATH.length - 1].orientation;
+	} else {
+		var player = Scene.getPlayer();
+		currentX = player.coord.x;
+		currentY = player.coord.y;
+		currentOrientation = player.orientation;
+	}
+	switch (currentOrientation) {
+		case 0:
+			nextX = currentX + 1;
+			nextY = currentY;
+			break;
+		case 2:
+			nextX = currentX - 1;
+			nextY = currentY;
+			break;
+		case 1:
+			nextX = currentX;
+			nextY = currentY - 1;
+			break;
+		case 3:
+			nextX = currentX;
+			nextY = currentY + 1;
+			break;
+	}
+	if (Scene.checkPointUsedByMonster(nextX, nextY)) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+Scene.checkPointUsedByBean = function (x, y) {
+	if (Scene.defaultDATA[y][x] == 0) {
+		return true;
+	}
+
+	return false;
+};
+
+Scene.checkPointUsedByMonster = function (x, y) {
+	if (x == Scene.monsterParams[0].x && y == Scene.monsterParams[0].y) {
+		return Scene.isFirstMonster;
+	} else if (x == Scene.monsterParams[1].x && y == Scene.monsterParams[1].y) {
+		return !Scene.isFirstMonster
+	} else {
+		return false;
+	}
+};
