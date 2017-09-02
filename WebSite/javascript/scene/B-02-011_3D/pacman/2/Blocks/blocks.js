b8ba7d311e62028720df8d03f1256e491eee8d36
willy <willy_j@163.com>
2017-09-02 15:02:00 +0800

    Add B-01-008, B-02-011

---
+++ WebSite/javascript/scene/B-02-011_3D/pacman/2/Blocks/blocks.js
@@ -0,0 +1,152 @@
+﻿'use strict';
+
+
+Blockly.Blocks['event_start'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("当开始运行");
+        this.setNextStatement(true, null);
+        this.setColour(150);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.JavaScript['event_start'] = function (block) {
+    //return 'Scene.start();\n';
+    return '';
+};
+
+
+Blockly.Blocks['turn_right'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("向右转");
+        this.setNextStatement(true, null);
+        this.setPreviousStatement(true, null);
+        this.setColour(150);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.JavaScript['turn_right'] = function (block) {
+    return 'Scene.TurnRight();';
+};
+
+Blockly.Blocks['turn_left'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("向左转");
+        this.setNextStatement(true, null);
+        this.setPreviousStatement(true, null);
+        this.setColour(150);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.JavaScript['turn_left'] = function (block) {
+    return 'Scene.TurnLeft();';
+};
+
+Blockly.Blocks['move_forward'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("向前移动")
+            // .appendField(new Blockly.FieldNumber(1, 1, 25, 1), "move_steps")
+            .appendField("1  步 ")
+        this.setNextStatement(true, null);
+        this.setPreviousStatement(true, null);
+        this.setColour(150);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.JavaScript['move_forward'] = function (block) {
+    // return 'Scene.move("", ' + block.getFieldValue('move_steps') + ');\n';
+    return 'Scene.move(1);\n';
+};
+
+Blockly.Blocks['move_nstep_forward'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("向前移动")
+        this.setNextStatement(true, null);
+        this.setPreviousStatement(true, null);
+        this.setColour(150);
+        this.setTooltip('');
+        this.setHelpUrl('http://www.example.com/');
+    }
+};
+
+Blockly.JavaScript['move_nstep_forward'] = function (block) {
+    return 'Scene.moveForward();\n';
+};
+
+
+Blockly.Blocks['if_condition'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("如果 ")
+        .appendField("前面是墙");
+    this.appendStatementInput("DO0")
+        .setCheck(null)
+        .appendField("执行");
+//    this.appendStatementInput("ELSE")
+//        .setCheck(null)
+//        .appendField("否则");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('');
+    this.setHelpUrl('');
+	var a = this;
+  }
+};
+
+Blockly.JavaScript['if_condition'] = function (a) {
+    var b = 0,
+		c = "",
+		d, e;
+    //do 
+		//e = Blockly.JavaScript.valueToCode(a, "IF" + b, Blockly.JavaScript.ORDER_NONE) || "false", 
+		d = Blockly.JavaScript.statementToCode(a, "DO" + b), 
+			c += (0 < b ? " else " : "") + "if (Scene.isWall()) {\nScene.setFunctionWhenWall('" + d + "')}";
+		//++b;
+	//while (a.getInput("IF" + b));
+  //  a.getInput("ELSE") && (d = Blockly.JavaScript.statementToCode(a, "ELSE"), c += " else {\n" + d + "}");
+    return c + ";\n"
+};
+
+
+Blockly.Blocks['forloop'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("重复")
+        .appendField(new Blockly.FieldTextInput("???"), "times")
+        .appendField("次");
+    this.appendStatementInput("do")
+        .setCheck(null)
+        .appendField("做");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+Blockly.JavaScript['forloop'] = function(block) {
+  var number_times = block.getFieldValue('times');
+  var statements_do = Blockly.JavaScript.statementToCode(block, 'do');
+  // TODO: Assemble JavaScript into code variable.
+  var countNum = "";
+  if(statements_do.match("for") != null){
+    var countNum = statements_do.match(/for/g).length;
+  }  
+  var count = "count" + countNum;
+  var code = 'for (var '+ count +' = 0; '+ count + ' < ' + number_times +'; '+ count + '++)\n {\n' + statements_do + ' }\n';
+  return code;
+};
