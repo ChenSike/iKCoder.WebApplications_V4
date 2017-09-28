1760e4f944341d0a22fcacf2cc85381bc519da66
CPLiu2012 <cpliu2001@sina.com>
2017-09-28 14:46:12 +0800

    .

---
+++ management/javascript/main.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+
+function initPage() {
+    $("#image_page_bg").css("height", $("body").height() + 'px');
+    $('#btn_Login').on('click', function () {
+        window.location.href = "main.html";
+    });
+}
---
+++ management/javascript/signin.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+function initPage() {
+    $("#image_page_bg").css("height", $("body").height() + 'px');
+    $('#btn_Login').on('click', function () {
+        window.location.href = "main.html";
+    });
+    $("#input-b9").fileinput({
+        showPreview: false,
+        showUpload: false,
+        elErrorContainer: '#kartik-file-errors',
+        allowedFileExtensions: ["jpg", "png", "gif"]
+    });
+
+}
