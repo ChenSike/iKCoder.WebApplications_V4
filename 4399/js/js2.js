fda7f1814c7856f15a5896f0a03a172e7b0f6e3d
CPLiu2012 <cpliu2001@sina.com>
2018-01-09 14:41:17 +0800

    .

---
+++ 4399/js/js2.js
@@ -0,0 +1,319 @@
+﻿function globalWrapFn (t) {
+    function e(t) {
+        var e = document.getElementsByTagName("head")[0],
+            i = document.createElement("script");
+        i.type = "text/javascript", i.charset = "utf-8", i.src = p.p + "" + t + "." + x + ".hot-update.js", e.appendChild(i);
+        console.log(i.src);
+    }
+
+    function i(t) {
+        if ("undefined" == typeof XMLHttpRequest) return t(new Error("No browser support"));
+        try {
+            var e = new XMLHttpRequest,
+                i = p.p + "" + x + ".hot-update.json";
+            e.open("GET", i, !0), e.timeout = 1e4, e.send(null)
+        } catch (n) {
+            return t(n)
+        }
+        e.onreadystatechange = function () {
+            if (4 === e.readyState)
+                if (0 === e.status) t(new Error("Manifest request to " + i + " timed out."));
+                else if (404 === e.status) t();
+                else if (200 !== e.status && 304 !== e.status) t(new Error("Manifest request to " + i + " failed."));
+                else {
+                    try {
+                        var n = JSON.parse(e.responseText)
+                    } catch (r) {
+                        return void t(r)
+                    }
+                    t(null, n)
+                }
+        }
+    }
+
+    function n(t) {
+        function e(t, e) {
+            "ready" === T && o("prepare"), E++, p.e(t, function () {
+                function i() {
+                    E--, "prepare" === T && (A[t] || l(t), 0 === E && 0 === M && c())
+                }
+                try {
+                    e.call(null, n)
+                } finally {
+                    i()
+                }
+            })
+        }
+        var i = R[t];
+        if (!i) return p;
+        var n = function (e) {
+            return i.hot.active ? R[e] ? (R[e].parents.indexOf(t) < 0 && R[e].parents.push(t), i.children.indexOf(e) < 0 && i.children.push(e)) : w = [t] : w = [], p(e)
+        };
+        for (var r in p) Object.prototype.hasOwnProperty.call(p, r) && (f ? Object.defineProperty(n, r, function (t) {
+            return {
+                configurable: !0,
+                enumerable: !0,
+                get: function () {
+                    return p[t]
+                },
+                set: function (e) {
+                    p[t] = e
+                }
+            }
+        }(r)) : n[r] = p[r]);
+        return f ? Object.defineProperty(n, "e", {
+            enumerable: !0,
+            value: e
+        }) : n.e = e, n
+    }
+
+    function r(t) {
+        var e = {
+            _acceptedDependencies: {},
+            _declinedDependencies: {},
+            _selfAccepted: !1,
+            _selfDeclined: !1,
+            _disposeHandlers: [],
+            active: !0,
+            accept: function (t, i) {
+                if ("undefined" == typeof t) e._selfAccepted = !0;
+                else if ("function" == typeof t) e._selfAccepted = t;
+                else if ("object" == typeof t)
+                    for (var n = 0; n < t.length; n++) e._acceptedDependencies[t[n]] = i;
+                else e._acceptedDependencies[t] = i
+            },
+            decline: function (t) {
+                if ("undefined" == typeof t) e._selfDeclined = !0;
+                else if ("number" == typeof t) e._declinedDependencies[t] = !0;
+                else
+                    for (var i = 0; i < t.length; i++) e._declinedDependencies[t[i]] = !0
+            },
+            dispose: function (t) {
+                e._disposeHandlers.push(t)
+            },
+            addDisposeHandler: function (t) {
+                e._disposeHandlers.push(t)
+            },
+            removeDisposeHandler: function (t) {
+                var i = e._disposeHandlers.indexOf(t);
+                i >= 0 && e._disposeHandlers.splice(i, 1)
+            },
+            check: a,
+            apply: u,
+            status: function (t) {
+                return t ? void S.push(t) : T
+            },
+            addStatusHandler: function (t) {
+                S.push(t)
+            },
+            removeStatusHandler: function (t) {
+                var e = S.indexOf(t);
+                e >= 0 && S.splice(e, 1)
+            },
+            data: b[t]
+        };
+        return e
+    }
+
+    function o(t) {
+        T = t;
+        for (var e = 0; e < S.length; e++) S[e].call(null, t)
+    }
+
+    function s(t) {
+        var e = +t + "" === t;
+        return e ? +t : t
+    }
+
+    function a(t, e) {
+        if ("idle" !== T) throw new Error("check() is only allowed in idle status");
+        "function" == typeof t ? (_ = !1, e = t) : (_ = t, e = e || function (t) {
+            if (t) throw t
+        }), o("check"), i(function (t, i) {
+            if (t) return e(t);
+            if (!i) return o("idle"), void e(null, null);
+            C = {}, L = {}, A = {};
+            for (var n = 0; n < i.c.length; n++) L[i.c[n]] = !0;
+            y = i.h, o("prepare"), g = e, v = {};
+            var r = 0;
+            l(r), "prepare" === T && 0 === E && 0 === M && c()
+        })
+    }
+
+    function h(t, e) {
+        if (L[t] && C[t]) {
+            C[t] = !1;
+            for (var i in e) Object.prototype.hasOwnProperty.call(e, i) && (v[i] = e[i]);
+            0 === --M && 0 === E && c()
+        }
+    }
+
+    function l(t) {
+        L[t] ? (C[t] = !0, M++, e(t)) : A[t] = !0
+    }
+
+    function c() {
+        o("ready");
+        var t = g;
+        if (g = null, t)
+            if (_) u(_, t);
+            else {
+                var e = [];
+                for (var i in v) Object.prototype.hasOwnProperty.call(v, i) && e.push(s(i));
+                t(null, e)
+            }
+    }
+
+    function u(e, i) {
+        function n(t) {
+            for (var e = [t], i = {}, n = e.slice() ; n.length > 0;) {
+                var o = n.pop(),
+                    t = R[o];
+                if (t && !t.hot._selfAccepted) {
+                    if (t.hot._selfDeclined) return new Error("Aborted because of self decline: " + o);
+                    if (0 === o) return;
+                    for (var s = 0; s < t.parents.length; s++) {
+                        var a = t.parents[s],
+                            h = R[a];
+                        if (h.hot._declinedDependencies[o]) return new Error("Aborted because of declined dependency: " + o + " in " + a);
+                        e.indexOf(a) >= 0 || (h.hot._acceptedDependencies[o] ? (i[a] || (i[a] = []), r(i[a], [o])) : (delete i[a], e.push(a), n.push(a)))
+                    }
+                }
+            }
+            return [e, i]
+        }
+
+        function r(t, e) {
+            for (var i = 0; i < e.length; i++) {
+                var n = e[i];
+                t.indexOf(n) < 0 && t.push(n)
+            }
+        }
+        if ("ready" !== T) throw new Error("apply() is only allowed in ready status");
+        "function" == typeof e ? (i = e, e = {}) : e && "object" == typeof e ? i = i || function (t) {
+            if (t) throw t
+        } : (e = {}, i = i || function (t) {
+            if (t) throw t
+        });
+        var a = {},
+            h = [],
+            l = {};
+        for (var c in v)
+            if (Object.prototype.hasOwnProperty.call(v, c)) {
+                var u = s(c),
+                    d = n(u);
+                if (!d) {
+                    if (e.ignoreUnaccepted) continue;
+                    return o("abort"), i(new Error("Aborted because " + u + " is not accepted"))
+                }
+                if (d instanceof Error) return o("abort"), i(d);
+                l[u] = v[u], r(h, d[0]);
+                for (var u in d[1]) Object.prototype.hasOwnProperty.call(d[1], u) && (a[u] || (a[u] = []), r(a[u], d[1][u]))
+            }
+        for (var f = [], m = 0; m < h.length; m++) {
+            var u = h[m];
+            R[u] && R[u].hot._selfAccepted && f.push({
+                module: u,
+                errorHandler: R[u].hot._selfAccepted
+            })
+        }
+        o("dispose");
+        for (var g = h.slice() ; g.length > 0;) {
+            var u = g.pop(),
+                _ = R[u];
+            if (_) {
+                for (var S = {}, M = _.hot._disposeHandlers, E = 0; E < M.length; E++) {
+                    var A = M[E];
+                    A(S)
+                }
+                b[u] = S, _.hot.active = !1, delete R[u];
+                for (var E = 0; E < _.children.length; E++) {
+                    var C = R[_.children[E]];
+                    if (C) {
+                        var L = C.parents.indexOf(u);
+                        L >= 0 && C.parents.splice(L, 1)
+                    }
+                }
+            }
+        }
+        for (var u in a)
+            if (Object.prototype.hasOwnProperty.call(a, u))
+                for (var _ = R[u], P = a[u], E = 0; E < P.length; E++) {
+                    var O = P[E],
+                        L = _.children.indexOf(O);
+                    L >= 0 && _.children.splice(L, 1)
+                }
+        o("apply"), x = y;
+        for (var u in l) Object.prototype.hasOwnProperty.call(l, u) && (t[u] = l[u]);
+        var I = null;
+        for (var u in a)
+            if (Object.prototype.hasOwnProperty.call(a, u)) {
+                for (var _ = R[u], P = a[u], D = [], m = 0; m < P.length; m++) {
+                    var O = P[m],
+                        A = _.hot._acceptedDependencies[O];
+                    D.indexOf(A) >= 0 || D.push(A)
+                }
+                for (var m = 0; m < D.length; m++) {
+                    var A = D[m];
+                    try {
+                        A(a)
+                    } catch (B) {
+                        I || (I = B)
+                    }
+                }
+            }
+        for (var m = 0; m < f.length; m++) {
+            var k = f[m],
+                u = k.module;
+            w = [u];
+            try {
+                p(u)
+            } catch (B) {
+                if ("function" == typeof k.errorHandler) try {
+                    k.errorHandler(B)
+                } catch (B) {
+                    I || (I = B)
+                } else I || (I = B)
+            }
+        }
+        return I ? (o("fail"), i(I)) : (o("idle"), void i(null, h))
+    }
+
+    function p(e) {
+        if (R[e]) return R[e].exports;
+        var i = R[e] = {
+            exports: {},
+            id: e,
+            loaded: !1,
+            hot: r(e),
+            parents: w,
+            children: []
+        };
+        return t[e].call(i.exports, i, i.exports, n(e)), i.loaded = !0, i.exports
+    }
+    var d = this.webpackHotUpdate;
+    this.webpackHotUpdate = function (t, e) {
+        h(t, e), d && d(t, e)
+    };
+    var f = !1;
+    try {
+        Object.defineProperty({}, "x", {
+            get: function () { }
+        }), f = !0
+    } catch (m) { }
+    var g, v, y, _ = !0,
+        x = "215b4e9087bad6b02f38",
+        b = {},
+        w = [],
+        S = [],
+        T = "idle",
+        M = 0,
+        E = 0,
+        A = {},
+        C = {},
+        L = {},
+        R = {};
+    return p.m = t, p.c = R, p.p = "", p.h = function () {
+        return x
+    }, n(0)(0)
+}
